d85bbc5ebc56fdd911da018cb87c72df9b232771
Cullen Jett <cullenjett@gmail.com>
2014-09-23 13:04:19 -0600

    add products helper

---
+++ app/helpers/products_helper.rb
@@ -0,0 +1,5 @@
+module ProductsHelper
+  def print_price(price)
+    number_to_currency(price)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'products#index'
   get 'ui(/:action)', controller: 'ui'
 
   resources :products, only: [:index, :new, :create]
