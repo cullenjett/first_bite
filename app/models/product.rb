0ab5280e18a645b6c26447ab064ae64f013773e2
Cullen Jett <cullenjett@gmail.com>
2014-09-23 16:02:43 -0600

    Product has_attached_file

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ActiveRecord::Base
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" },
+                            default_url: "public/stock_product.jpg"
+                            
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
   validates :title,         presence: true,
                             uniqueness: true
 
